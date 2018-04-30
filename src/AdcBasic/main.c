f40f6433ed8b31150d7c3bc92db1653171fde50a
Wootaik Lee <wootaik@gmail.com>
2018-05-01 00:35:06 +0900

    Add AdcBasic

---
+++ src/AdcBasic/main.c
@@ -0,0 +1,58 @@
+/*
+ * main.c
+ *
+ *  Created on: 2016 Dec 19 00:39:23
+ *  Author: Wootaik
+ */
+
+
+
+
+#include <DAVE.h>                 //Declarations from DAVE Code Generation (includes SFR declaration)
+
+/**
+
+ * @brief main() - Application entry point
+ *
+ * <b>Details of function</b><br>
+ * This routine is the application entry point. It is invoked by the device startup code. It is responsible for
+ * invoking the APP initialization dispatcher routine - DAVE_Init() and hosting the place-holder for user application
+ * code.
+ */
+
+int main(void)
+{
+  DAVE_STATUS_t status;
+
+  status = DAVE_Init();           /* Initialization of DAVE APPs  */
+
+  if(status != DAVE_STATUS_SUCCESS)
+  {
+    /* Placeholder for error handler code. The while loop below can be replaced with an user error handler. */
+    XMC_DEBUG("DAVE APPs initialization failed\n");
+
+    while(1U)
+    {
+
+    }
+  }
+
+  ADC_MEASUREMENT_StartConversion(&dhADC_MEASUREMENT_0);
+
+  /* Placeholder for user application code. The while loop below can be replaced with user application code. */
+  while(1U)
+  {
+
+  }
+}
+
+void ISR_dhADC_MEASUREMENT_0()
+{
+	uint32_t channel_a;
+	uint32_t channel_b;
+	channel_a = ADC_MEASUREMENT_GetResult(&ADC_MEASUREMENT_Channel_A);
+	channel_b = ADC_MEASUREMENT_GetResult(&ADC_MEASUREMENT_Channel_B);
+	ADC_MEASUREMENT_StartConversion(&dhADC_MEASUREMENT_0);
+}
+
+
