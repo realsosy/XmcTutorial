55d8ba55a4557aaadff289fc5699a0ac92688940
Wootaik Lee <wootaik@gmail.com>
2018-11-12 10:23:16 +0900

    Add SpiAdxl345

---
+++ src/SpiADXL345/ADXL345.h
@@ -0,0 +1,94 @@
+/*
+ADXL345 Library Main Header File
+*/
+
+
+#ifndef ADXL345_h
+#define ADXL345_h
+
+/*************************** REGISTER MAP ***************************/
+#define ADXL345_DEVID			0x00		// Device ID
+#define ADXL345_RESERVED1		0x01		// Reserved. Do Not Access. 
+#define ADXL345_THRESH_TAP		0x1D		// Tap Threshold. 
+#define ADXL345_OFSX			0x1E		// X-Axis Offset. 
+#define ADXL345_OFSY			0x1F		// Y-Axis Offset.
+#define ADXL345_OFSZ			0x20		// Z- Axis Offset.
+#define ADXL345_DUR				0x21		// Tap Duration.
+#define ADXL345_LATENT			0x22		// Tap Latency.
+#define ADXL345_WINDOW			0x23		// Tap Window.
+#define ADXL345_THRESH_ACT		0x24		// Activity Threshold
+#define ADXL345_THRESH_INACT	0x25		// Inactivity Threshold
+#define ADXL345_TIME_INACT		0x26		// Inactivity Time
+#define ADXL345_ACT_INACT_CTL	0x27		// Axis Enable Control for Activity and Inactivity Detection
+#define ADXL345_THRESH_FF		0x28		// Free-Fall Threshold.
+#define ADXL345_TIME_FF			0x29		// Free-Fall Time.
+#define ADXL345_TAP_AXES		0x2A		// Axis Control for Tap/Double Tap.
+#define ADXL345_ACT_TAP_STATUS	0x2B		// Source of Tap/Double Tap
+#define ADXL345_BW_RATE			0x2C		// Data Rate and Power mode Control
+#define ADXL345_POWER_CTL		0x2D		// Power-Saving Features Control
+#define ADXL345_INT_ENABLE		0x2E		// Interrupt Enable Control
+#define ADXL345_INT_MAP			0x2F		// Interrupt Mapping Control
+#define ADXL345_INT_SOURCE		0x30		// Source of Interrupts
+#define ADXL345_DATA_FORMAT		0x31		// Data Format Control
+#define ADXL345_DATAX0			0x32		// X-Axis Data 0
+#define ADXL345_DATAX1			0x33		// X-Axis Data 1
+#define ADXL345_DATAY0			0x34		// Y-Axis Data 0
+#define ADXL345_DATAY1			0x35		// Y-Axis Data 1
+#define ADXL345_DATAZ0			0x36		// Z-Axis Data 0
+#define ADXL345_DATAZ1			0x37		// Z-Axis Data 1
+#define ADXL345_FIFO_CTL		0x38		// FIFO Control
+#define ADXL345_FIFO_STATUS		0x39		// FIFO Status
+
+#define ADXL345_BW_1600			0xF			// 1111		IDD = 40uA
+#define ADXL345_BW_800			0xE			// 1110		IDD = 90uA
+#define ADXL345_BW_400			0xD			// 1101		IDD = 140uA
+#define ADXL345_BW_200			0xC			// 1100		IDD = 140uA
+#define ADXL345_BW_100			0xB			// 1011		IDD = 140uA 
+#define ADXL345_BW_50			0xA			// 1010		IDD = 140uA
+#define ADXL345_BW_25			0x9			// 1001		IDD = 90uA
+#define ADXL345_BW_12_5		    0x8			// 1000		IDD = 60uA 
+#define ADXL345_BW_6_25			0x7			// 0111		IDD = 50uA
+#define ADXL345_BW_3_13			0x6			// 0110		IDD = 45uA
+#define ADXL345_BW_1_56			0x5			// 0101		IDD = 40uA
+#define ADXL345_BW_0_78			0x4			// 0100		IDD = 34uA
+#define ADXL345_BW_0_39			0x3			// 0011		IDD = 23uA
+#define ADXL345_BW_0_20			0x2			// 0010		IDD = 23uA
+#define ADXL345_BW_0_10			0x1			// 0001		IDD = 23uA
+#define ADXL345_BW_0_05			0x0			// 0000		IDD = 23uA
+
+
+ /************************** INTERRUPT PINS **************************/
+#define ADXL345_INT1_PIN		0x00		//INT1: 0
+#define ADXL345_INT2_PIN		0x01		//INT2: 1
+
+
+ /********************** INTERRUPT BIT POSITION **********************/
+#define ADXL345_INT_DATA_READY_BIT		0x07
+#define ADXL345_INT_SINGLE_TAP_BIT		0x06
+#define ADXL345_INT_DOUBLE_TAP_BIT		0x05
+#define ADXL345_INT_ACTIVITY_BIT		0x04
+#define ADXL345_INT_INACTIVITY_BIT		0x03
+#define ADXL345_INT_FREE_FALL_BIT		0x02
+#define ADXL345_INT_WATERMARK_BIT		0x01
+#define ADXL345_INT_OVERRUNY_BIT		0x00
+
+#define ADXL345_DATA_READY				0x07
+#define ADXL345_SINGLE_TAP				0x06
+#define ADXL345_DOUBLE_TAP				0x05
+#define ADXL345_ACTIVITY				0x04
+#define ADXL345_INACTIVITY				0x03
+#define ADXL345_FREE_FALL				0x02
+#define ADXL345_WATERMARK				0x01
+#define ADXL345_OVERRUNY				0x00
+
+
+ /****************************** ERRORS ******************************/
+#define ADXL345_OK			1		// No Error
+#define ADXL345_ERROR		0		// Error Exists
+
+#define ADXL345_NO_ERROR	0		// Initial State
+#define ADXL345_READ_ERROR	1		// Accelerometer Reading Error
+#define ADXL345_BAD_ARG		2		// Bad Argument
+
+
+#endif
---
+++ src/SpiADXL345/main.c
@@ -0,0 +1,203 @@
+/*
+ * main.c
+ *
+ *  Created on: 2017 Aug 06 23:19:56
+ *  Author: Wootaik
+ */
+
+
+
+
+#include <DAVE.h>                 //Declarations from DAVE Code Generation (includes SFR declaration)
+
+#include "ADXL345.h"
+
+/**
+
+ * @brief main() - Application entry point
+ *
+ * <b>Details of function</b><br>
+ * This routine is the application entry point. It is invoked by the device startup code. It is responsible for
+ * invoking the APP initialization dispatcher routine - DAVE_Init() and hosting the place-holder for user application
+ * code.
+ */
+#define BUFFER_SIZE                  12
+
+
+uint8_t FrameByte = 2;
+
+/*Data for the Master*/
+uint8_t MasterDataTx[BUFFER_SIZE] = {1};
+uint8_t MasterDataRx[BUFFER_SIZE] = {2};
+uint8_t MasterTxEnable = false;
+uint8_t MasterRxEnable = false;
+uint8_t MasterTransEnable = false;
+
+uint32_t SysTimer100mId;
+bool SysTimer100mFlag=false;
+
+bool bXyzComplete = false;
+int16_t AccelX;
+int16_t AccelY;
+int16_t AccelZ;
+bool AccelScanEnable = true;
+
+void ADXL345_WriteRegister(uint8_t reg, uint8_t value);
+uint8_t ADXL345_ReadRegister(uint8_t reg);
+bool ADXL345_Begin(void);
+
+void ISR_SpiMasterRx(){
+	//DIGITAL_IO_ToggleOutput(&dhDO_SPI_MASTER);
+
+	// Get XYZ using ISR
+	if(bXyzComplete==false) {
+		AccelX = (int16_t)( ((uint16_t)MasterDataRx[3] << 8) | MasterDataRx[1] );
+		AccelY = (int16_t)( ((uint16_t)MasterDataRx[7] << 8) | MasterDataRx[5] );
+		AccelZ = (int16_t)( ((uint16_t)MasterDataRx[11] << 8) | MasterDataRx[9] );
+		bXyzComplete = true;
+	}
+}
+
+void ADXL345_WriteRegister(uint8_t reg, uint8_t value){
+	MasterDataTx[0] = 0x00 | reg;
+	MasterDataTx[1] = value;
+	while(SPI_MASTER_IsTxBusy(&dhSPI_MASTER))  {  }
+	SPI_MASTER_Transfer(&dhSPI_MASTER, MasterDataTx, MasterDataRx, FrameByte);
+}
+
+uint8_t ADXL345_ReadRegister(uint8_t reg){
+	MasterDataTx[0] = 0x80 | reg;
+	MasterDataTx[1] = 0xFF;
+	SPI_MASTER_Transfer(&dhSPI_MASTER, MasterDataTx, MasterDataRx, FrameByte);
+	while(SPI_MASTER_IsRxBusy(&dhSPI_MASTER))  {  } /*Wait until MasterDataRx updated */
+	return(MasterDataRx[1]);
+}
+
+bool ADXL345_Begin(void){
+	uint8_t device_id;
+
+	/* Get & Check DEVID */
+	device_id = ADXL345_ReadRegister(ADXL345_DEVID);
+	if(device_id != 0xE5)
+	{
+		return false;
+	}
+
+	ADXL345_WriteRegister(ADXL345_BW_RATE, 0x0D); /* Set Conversion Rate 800Hz */
+
+	ADXL345_WriteRegister(ADXL345_POWER_CTL, 0x08); /* Enable measurements */
+
+	return(true);
+}
+
+void ADXL345_GetXYZ(void){
+	// Get XYZ using ISR
+	MasterDataTx[0] = 0x80|ADXL345_DATAX0;
+	MasterDataTx[1] = 0;
+	MasterDataTx[2] = 0x80|ADXL345_DATAX1;
+	MasterDataTx[3] = 0;
+	MasterDataTx[4] = 0x80|ADXL345_DATAY0;
+	MasterDataTx[5] = 0;
+	MasterDataTx[6] = 0x80|ADXL345_DATAY1;
+	MasterDataTx[7] = 0;
+	MasterDataTx[8] = 0x80|ADXL345_DATAZ0;
+	MasterDataTx[9] = 0;
+	MasterDataTx[10] = 0x80|ADXL345_DATAZ1;
+	MasterDataTx[11] = 0;
+
+	bXyzComplete = false;
+	SPI_MASTER_Transfer(&dhSPI_MASTER, MasterDataTx, MasterDataRx, FrameByte*6);
+}
+
+void ADXL345_GetXYZ_Polling(void){
+	uint16_t upper, lower;
+	lower = ADXL345_ReadRegister(ADXL345_DATAX0);
+	upper = ADXL345_ReadRegister(ADXL345_DATAX1);
+	AccelX = (int16_t) ( (upper << 8) | lower);
+	lower = ADXL345_ReadRegister(ADXL345_DATAY0);
+	upper = ADXL345_ReadRegister(ADXL345_DATAY1);
+	AccelY = (int16_t) ( (upper << 8) | lower);
+	lower = ADXL345_ReadRegister(ADXL345_DATAZ0);
+	upper = ADXL345_ReadRegister(ADXL345_DATAZ1);
+	AccelZ = (int16_t) ( (upper << 8) | lower);
+}
+
+void TestAdxl345(){
+
+	// Get XYZ using ISR
+	ADXL345_GetXYZ();
+
+	// Get XYZ using Polling
+	//ADXL345_GetXYZ_Polling();
+
+	if(AccelX >= 0){
+		PWM_SetDutyCycle(&dhPWM_XPlus, AccelX<<4);
+		PWM_SetDutyCycle(&dhPWM_XMinus, 0);
+	}
+	else{
+		PWM_SetDutyCycle(&dhPWM_XPlus, 0);
+		PWM_SetDutyCycle(&dhPWM_XMinus, (-1*AccelX)<<4);
+	}
+}
+
+void SysTimer100m(){
+	SysTimer100mFlag = true;
+}
+
+int main(void)
+{
+	DAVE_STATUS_t status;
+
+	status = DAVE_Init();           /* Initialization of DAVE APPs  */
+
+	if(status != DAVE_STATUS_SUCCESS)
+	{
+		/* Placeholder for error handler code. The while loop below can be replaced with an user error handler. */
+		XMC_DEBUG("DAVE APPs initialization failed\n");
+
+		while(1U)
+		{
+
+		}
+	}
+
+	/* Placeholder for user application code. The while loop below can be replaced with user application code. */
+
+	SPI_MASTER_DisableSlaveSelectSignal(&dhSPI_MASTER);
+	SPI_MASTER_EnableSlaveSelectSignal(&dhSPI_MASTER, SPI_MASTER_SS_SIGNAL_0);
+
+	ADXL345_Begin();
+
+	SysTimer100mId = SYSTIMER_CreateTimer(100000, SYSTIMER_MODE_PERIODIC, SysTimer100m, NULL);
+
+	SYSTIMER_StartTimer(SysTimer100mId);
+
+	while(1U)
+	{
+		if(MasterTxEnable == true){
+			while(SPI_MASTER_IsTxBusy(&dhSPI_MASTER))  {  }
+			SPI_MASTER_Transmit(&dhSPI_MASTER, MasterDataTx, FrameByte);
+			MasterTxEnable = false;
+		}
+
+		if(MasterRxEnable == true){
+			while(SPI_MASTER_IsRxBusy(&dhSPI_MASTER))  {  }
+			SPI_MASTER_Receive(&dhSPI_MASTER, MasterDataRx, FrameByte);
+			MasterRxEnable = false;
+		}
+
+		if(MasterTransEnable == true){
+			while(SPI_MASTER_IsTxBusy(&dhSPI_MASTER))  {  }
+			SPI_MASTER_Transfer(&dhSPI_MASTER, MasterDataTx, MasterDataRx, FrameByte);
+			MasterTransEnable = false;
+		}
+
+		if(SysTimer100mFlag == true){
+			if (AccelScanEnable == true){
+				TestAdxl345();
+			}
+			SysTimer100mFlag = false;
+		}
+
+	}
+}
